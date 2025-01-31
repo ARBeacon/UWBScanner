c65ff9133ccbb21f98b8aaf69b83641714d3d055
Matthew <52621525+H11Maitree@users.noreply.github.com>
2025-01-31 16:58:57 +0700

    Initial Commit

---
+++ UWBScanner/UWBScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UWBScannerApp.swift
+//  UWBScanner
+//
+//  Created by Maitree Hirunteeyakul on 31/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct UWBScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UWBScannerTests/UWBScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  UWBScannerTests.swift
+//  UWBScannerTests
+//
+//  Created by Maitree Hirunteeyakul on 31/01/2025.
+//
+
+import Testing
+@testable import UWBScanner
+
+struct UWBScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
